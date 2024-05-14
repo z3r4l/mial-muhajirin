8b664567930bdeebec120b9a18220aea5dc3d48e
Zery Aldi <zeri.aldi21@gmail.com>
2024-05-14 20:02:23 +0700

    first commit

---
+++ sekolah.sql
@@ -0,0 +1,578 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 14, 2024 at 01:07 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sekolah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_agenda`
+--
+
+CREATE TABLE `tbl_agenda` (
+  `agenda_id` int(11) NOT NULL,
+  `agenda_nama` varchar(200) DEFAULT NULL,
+  `agenda_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `agenda_deskripsi` text,
+  `agenda_mulai` date DEFAULT NULL,
+  `agenda_selesai` date DEFAULT NULL,
+  `agenda_tempat` varchar(90) DEFAULT NULL,
+  `agenda_waktu` varchar(30) DEFAULT NULL,
+  `agenda_keterangan` varchar(200) DEFAULT NULL,
+  `agenda_author` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_agenda`
+--
+
+INSERT INTO `tbl_agenda` (`agenda_id`, `agenda_nama`, `agenda_tanggal`, `agenda_deskripsi`, `agenda_mulai`, `agenda_selesai`, `agenda_tempat`, `agenda_waktu`, `agenda_keterangan`, `agenda_author`) VALUES
+(3, 'Penerimaan Raport Semester Ganjil Tahun Ajaran 2017-2018', '2017-01-22 06:29:49', 'Berakhirnya semester ganjil tahun pelajaran 2016-2017, ditandai dengan pembagian laporan hasil belajar.', '2017-02-17', '2017-02-17', 'M-Sekolah', '07.30 - 12.00 WIB', 'Untuk kelas XI dan XII, pembagian raport dimulai pukul 07.30 WIB. Sedangkan untuk kelas X pada pukul 09.00 WIB. Raport diambil oleh orang tua/wali murid masing-masing.', 'M Fikri Setiadi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_album`
+--
+
+CREATE TABLE `tbl_album` (
+  `album_id` int(11) NOT NULL,
+  `album_nama` varchar(50) DEFAULT NULL,
+  `album_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `album_pengguna_id` int(11) DEFAULT NULL,
+  `album_author` varchar(60) DEFAULT NULL,
+  `album_count` int(11) DEFAULT '0',
+  `album_cover` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_files`
+--
+
+CREATE TABLE `tbl_files` (
+  `file_id` int(11) NOT NULL,
+  `file_judul` varchar(120) DEFAULT NULL,
+  `file_deskripsi` text,
+  `file_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `file_oleh` varchar(60) DEFAULT NULL,
+  `file_download` int(11) DEFAULT '0',
+  `file_data` varchar(120) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_galeri`
+--
+
+CREATE TABLE `tbl_galeri` (
+  `galeri_id` int(11) NOT NULL,
+  `galeri_judul` varchar(60) DEFAULT NULL,
+  `galeri_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `galeri_gambar` varchar(40) DEFAULT NULL,
+  `galeri_album_id` int(11) DEFAULT NULL,
+  `galeri_pengguna_id` int(11) DEFAULT NULL,
+  `galeri_author` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_guru`
+--
+
+CREATE TABLE `tbl_guru` (
+  `guru_id` int(11) NOT NULL,
+  `guru_nip` varchar(30) DEFAULT NULL,
+  `guru_nama` varchar(70) DEFAULT NULL,
+  `guru_jenkel` varchar(2) DEFAULT NULL,
+  `guru_tmp_lahir` varchar(80) DEFAULT NULL,
+  `guru_tgl_lahir` varchar(80) DEFAULT NULL,
+  `guru_mapel` varchar(120) DEFAULT NULL,
+  `guru_photo` varchar(40) DEFAULT NULL,
+  `guru_tgl_input` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_inbox`
+--
+
+CREATE TABLE `tbl_inbox` (
+  `inbox_id` int(11) NOT NULL,
+  `inbox_nama` varchar(40) DEFAULT NULL,
+  `inbox_email` varchar(60) DEFAULT NULL,
+  `inbox_kontak` varchar(20) DEFAULT NULL,
+  `inbox_pesan` text,
+  `inbox_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `inbox_status` int(11) DEFAULT '1' COMMENT '1=Belum dilihat, 0=Telah dilihat'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `kategori_id` int(11) NOT NULL,
+  `kategori_nama` varchar(30) DEFAULT NULL,
+  `kategori_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_kategori`
+--
+
+INSERT INTO `tbl_kategori` (`kategori_id`, `kategori_nama`, `kategori_tanggal`) VALUES
+(14, 'daftarmatapelajaran', '2024-03-19 05:12:40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kelas`
+--
+
+CREATE TABLE `tbl_kelas` (
+  `kelas_id` int(11) NOT NULL,
+  `kelas_nama` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_kelas`
+--
+
+INSERT INTO `tbl_kelas` (`kelas_id`, `kelas_nama`) VALUES
+(26, 'Kelas I.A'),
+(27, 'Kelas I.B'),
+(28, 'Kelas II.A'),
+(29, 'Kelas II.B'),
+(30, 'Kelas III.A'),
+(31, 'Kelas III.B'),
+(32, 'Kelas IV.A'),
+(33, 'Kelas IV.B'),
+(34, 'Kelas V.A'),
+(35, 'Kelas V.B'),
+(36, 'Kelas VI.A'),
+(37, 'Kelas VI.B'),
+(38, 'Kelas I.A'),
+(39, 'Kelas I.B'),
+(40, 'Kelas II.A'),
+(41, 'Kelas II.B'),
+(42, 'Kelas III.A'),
+(43, 'Kelas III.B'),
+(44, 'Kelas IV.A'),
+(45, 'Kelas IV.B'),
+(46, 'Kelas V.A'),
+(47, 'Kelas V.B'),
+(48, 'Kelas VI.A'),
+(49, 'Kelas VI.B');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_komentar`
+--
+
+CREATE TABLE `tbl_komentar` (
+  `komentar_id` int(11) NOT NULL,
+  `komentar_nama` varchar(30) DEFAULT NULL,
+  `komentar_email` varchar(50) DEFAULT NULL,
+  `komentar_isi` varchar(120) DEFAULT NULL,
+  `komentar_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `komentar_status` varchar(2) DEFAULT NULL,
+  `komentar_tulisan_id` int(11) DEFAULT NULL,
+  `komentar_parent` int(11) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_komentar`
+--
+
+INSERT INTO `tbl_komentar` (`komentar_id`, `komentar_nama`, `komentar_email`, `komentar_isi`, `komentar_tanggal`, `komentar_status`, `komentar_tulisan_id`, `komentar_parent`) VALUES
+(4, 'viliyan intan pratama', 'admin@laundry.com', ' ada info engak', '2024-01-08 05:31:18', '1', 24, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_log_aktivitas`
+--
+
+CREATE TABLE `tbl_log_aktivitas` (
+  `log_id` int(11) NOT NULL,
+  `log_nama` text,
+  `log_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `log_ip` varchar(20) DEFAULT NULL,
+  `log_pengguna_id` int(11) DEFAULT NULL,
+  `log_icon` blob,
+  `log_jenis_icon` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pengguna`
+--
+
+CREATE TABLE `tbl_pengguna` (
+  `pengguna_id` int(11) NOT NULL,
+  `pengguna_nama` varchar(50) DEFAULT NULL,
+  `pengguna_moto` varchar(100) DEFAULT NULL,
+  `pengguna_jenkel` varchar(2) DEFAULT NULL,
+  `pengguna_username` varchar(30) DEFAULT NULL,
+  `pengguna_password` varchar(35) DEFAULT NULL,
+  `pengguna_tentang` text,
+  `pengguna_email` varchar(50) DEFAULT NULL,
+  `pengguna_nohp` varchar(20) DEFAULT NULL,
+  `pengguna_facebook` varchar(35) DEFAULT NULL,
+  `pengguna_twitter` varchar(35) DEFAULT NULL,
+  `pengguna_linkdin` varchar(35) DEFAULT NULL,
+  `pengguna_google_plus` varchar(35) DEFAULT NULL,
+  `pengguna_status` int(2) DEFAULT '1',
+  `pengguna_level` varchar(3) DEFAULT NULL,
+  `pengguna_register` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `pengguna_photo` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_pengguna`
+--
+
+INSERT INTO `tbl_pengguna` (`pengguna_id`, `pengguna_nama`, `pengguna_moto`, `pengguna_jenkel`, `pengguna_username`, `pengguna_password`, `pengguna_tentang`, `pengguna_email`, `pengguna_nohp`, `pengguna_facebook`, `pengguna_twitter`, `pengguna_linkdin`, `pengguna_google_plus`, `pengguna_status`, `pengguna_level`, `pengguna_register`, `pengguna_photo`) VALUES
+(2, 'Haidir', NULL, 'L', 'admin', '21232f297a57a5a743894a0e4a801fc3', NULL, 'dirh3845@gmail.com', '081378017271', NULL, NULL, NULL, NULL, 1, '1', '2024-05-14 10:33:29', '13858c0d464c0fb1513fc0bd160fe7d5.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pengumuman`
+--
+
+CREATE TABLE `tbl_pengumuman` (
+  `pengumuman_id` int(11) NOT NULL,
+  `pengumuman_judul` varchar(150) DEFAULT NULL,
+  `pengumuman_deskripsi` text,
+  `pengumuman_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `pengumuman_author` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_pengumuman`
+--
+
+INSERT INTO `tbl_pengumuman` (`pengumuman_id`, `pengumuman_judul`, `pengumuman_deskripsi`, `pengumuman_tanggal`, `pengumuman_author`) VALUES
+(1, 'Pengumuman Libur Semester Ganjil Tahun Ajaran 2016-2017', 'Libur semester ganjil tahun ajaran 2016-2017 dimulai dari tanggal 3 Maret 2017 sampai dengan tanggal 7 Maret 207.', '2017-01-21 01:17:30', 'M Fikri Setiadi'),
+(2, 'Pengumuman Pembagian Raport Semester Ganjil Tahun Ajaran 2016-2017', 'Menjelang berakhirnya proses belajar-mengajar di semester ganjil tahun ajaran 2016-2017, maka akan diadakan pembagian hasil belajar/raport pada tanggal 4 Maret 2017 pukul 07.30 WIB.\r\nYang bertempat di M-Sekolah. Raport diambil oleh orang tua/wali kelas murid masing-masing', '2017-01-21 01:16:20', 'M Fikri Setiadi'),
+(3, 'Pengumuman Peresmian dan Launching Website Perdana M-Sekolah', 'Peresmian dan launching website resmi M-Sekolah akan diadakan pada hari 23 Desember 2016 pukul 10.00, bertepatan dengan pembagian raport semester ganjil tahun ajaran 2016-2017', '2017-01-22 07:16:16', 'M Fikri Setiadi'),
+(4, 'Pengumuman Proses Belajar Mengajar di Semester Genap Tahun Ajaran 2016-2017', 'Setelah libur semester ganjil tahun ajaran 2016-2017, proses belajar mengajar di semester genap tahun ajaran 2016-2017 mulai aktif kembali tanggal 2 Maret 2017.', '2017-01-22 07:15:28', 'M Fikri Setiadi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pengunjung`
+--
+
+CREATE TABLE `tbl_pengunjung` (
+  `pengunjung_id` int(11) NOT NULL,
+  `pengunjung_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `pengunjung_ip` varchar(40) DEFAULT NULL,
+  `pengunjung_perangkat` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_siswa`
+--
+
+CREATE TABLE `tbl_siswa` (
+  `siswa_id` int(11) NOT NULL,
+  `siswa_nis` varchar(20) DEFAULT NULL,
+  `siswa_nama` varchar(70) DEFAULT NULL,
+  `siswa_jenkel` varchar(2) DEFAULT NULL,
+  `siswa_kelas_id` int(11) DEFAULT NULL,
+  `siswa_photo` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_siswa`
+--
+
+INSERT INTO `tbl_siswa` (`siswa_id`, `siswa_nis`, `siswa_nama`, `siswa_jenkel`, `siswa_kelas_id`, `siswa_photo`) VALUES
+(15, '5777', 'TES', 'L', 27, NULL),
+(16, '777', 'TES2', 'P', 28, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_testimoni`
+--
+
+CREATE TABLE `tbl_testimoni` (
+  `testimoni_id` int(11) NOT NULL,
+  `testimoni_nama` varchar(30) DEFAULT NULL,
+  `testimoni_isi` varchar(120) DEFAULT NULL,
+  `testimoni_email` varchar(35) DEFAULT NULL,
+  `testimoni_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_tulisan`
+--
+
+CREATE TABLE `tbl_tulisan` (
+  `tulisan_id` int(11) NOT NULL,
+  `tulisan_judul` varchar(100) DEFAULT NULL,
+  `tulisan_isi` text,
+  `tulisan_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `tulisan_kategori_id` int(11) DEFAULT NULL,
+  `tulisan_kategori_nama` varchar(30) DEFAULT NULL,
+  `tulisan_views` int(11) DEFAULT '0',
+  `tulisan_gambar` varchar(40) DEFAULT NULL,
+  `tulisan_pengguna_id` int(11) DEFAULT NULL,
+  `tulisan_author` varchar(40) DEFAULT NULL,
+  `tulisan_img_slider` int(2) NOT NULL DEFAULT '0',
+  `tulisan_slug` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_tulisan`
+--
+
+INSERT INTO `tbl_tulisan` (`tulisan_id`, `tulisan_judul`, `tulisan_isi`, `tulisan_tanggal`, `tulisan_kategori_id`, `tulisan_kategori_nama`, `tulisan_views`, `tulisan_gambar`, `tulisan_pengguna_id`, `tulisan_author`, `tulisan_img_slider`, `tulisan_slug`) VALUES
+(30, 'DAFTAR MATA PELAJARAN SISWA', '<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n', '2024-05-14 09:50:40', 14, 'daftarmatapelajaran', 11, 'd251b7e566055504493f772ad0e87cf5.jpg', 1, 'M Fikri Setiadi', 0, 'daftar-mata-pelajaran-siswa');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_agenda`
+--
+ALTER TABLE `tbl_agenda`
+  ADD PRIMARY KEY (`agenda_id`);
+
+--
+-- Indexes for table `tbl_album`
+--
+ALTER TABLE `tbl_album`
+  ADD PRIMARY KEY (`album_id`),
+  ADD KEY `album_pengguna_id` (`album_pengguna_id`);
+
+--
+-- Indexes for table `tbl_files`
+--
+ALTER TABLE `tbl_files`
+  ADD PRIMARY KEY (`file_id`);
+
+--
+-- Indexes for table `tbl_galeri`
+--
+ALTER TABLE `tbl_galeri`
+  ADD PRIMARY KEY (`galeri_id`),
+  ADD KEY `galeri_album_id` (`galeri_album_id`),
+  ADD KEY `galeri_pengguna_id` (`galeri_pengguna_id`);
+
+--
+-- Indexes for table `tbl_guru`
+--
+ALTER TABLE `tbl_guru`
+  ADD PRIMARY KEY (`guru_id`);
+
+--
+-- Indexes for table `tbl_inbox`
+--
+ALTER TABLE `tbl_inbox`
+  ADD PRIMARY KEY (`inbox_id`);
+
+--
+-- Indexes for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`kategori_id`);
+
+--
+-- Indexes for table `tbl_kelas`
+--
+ALTER TABLE `tbl_kelas`
+  ADD PRIMARY KEY (`kelas_id`);
+
+--
+-- Indexes for table `tbl_komentar`
+--
+ALTER TABLE `tbl_komentar`
+  ADD PRIMARY KEY (`komentar_id`),
+  ADD KEY `komentar_tulisan_id` (`komentar_tulisan_id`);
+
+--
+-- Indexes for table `tbl_log_aktivitas`
+--
+ALTER TABLE `tbl_log_aktivitas`
+  ADD PRIMARY KEY (`log_id`),
+  ADD KEY `log_pengguna_id` (`log_pengguna_id`);
+
+--
+-- Indexes for table `tbl_pengguna`
+--
+ALTER TABLE `tbl_pengguna`
+  ADD PRIMARY KEY (`pengguna_id`);
+
+--
+-- Indexes for table `tbl_pengumuman`
+--
+ALTER TABLE `tbl_pengumuman`
+  ADD PRIMARY KEY (`pengumuman_id`);
+
+--
+-- Indexes for table `tbl_pengunjung`
+--
+ALTER TABLE `tbl_pengunjung`
+  ADD PRIMARY KEY (`pengunjung_id`);
+
+--
+-- Indexes for table `tbl_siswa`
+--
+ALTER TABLE `tbl_siswa`
+  ADD PRIMARY KEY (`siswa_id`);
+
+--
+-- Indexes for table `tbl_testimoni`
+--
+ALTER TABLE `tbl_testimoni`
+  ADD PRIMARY KEY (`testimoni_id`);
+
+--
+-- Indexes for table `tbl_tulisan`
+--
+ALTER TABLE `tbl_tulisan`
+  ADD PRIMARY KEY (`tulisan_id`),
+  ADD KEY `tulisan_kategori_id` (`tulisan_kategori_id`),
+  ADD KEY `tulisan_pengguna_id` (`tulisan_pengguna_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_agenda`
+--
+ALTER TABLE `tbl_agenda`
+  MODIFY `agenda_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_album`
+--
+ALTER TABLE `tbl_album`
+  MODIFY `album_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_files`
+--
+ALTER TABLE `tbl_files`
+  MODIFY `file_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `tbl_galeri`
+--
+ALTER TABLE `tbl_galeri`
+  MODIFY `galeri_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_guru`
+--
+ALTER TABLE `tbl_guru`
+  MODIFY `guru_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tbl_inbox`
+--
+ALTER TABLE `tbl_inbox`
+  MODIFY `inbox_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `kategori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `tbl_kelas`
+--
+ALTER TABLE `tbl_kelas`
+  MODIFY `kelas_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+
+--
+-- AUTO_INCREMENT for table `tbl_komentar`
+--
+ALTER TABLE `tbl_komentar`
+  MODIFY `komentar_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tbl_log_aktivitas`
+--
+ALTER TABLE `tbl_log_aktivitas`
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_pengguna`
+--
+ALTER TABLE `tbl_pengguna`
+  MODIFY `pengguna_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_pengumuman`
+--
+ALTER TABLE `tbl_pengumuman`
+  MODIFY `pengumuman_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tbl_pengunjung`
+--
+ALTER TABLE `tbl_pengunjung`
+  MODIFY `pengunjung_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=941;
+
+--
+-- AUTO_INCREMENT for table `tbl_siswa`
+--
+ALTER TABLE `tbl_siswa`
+  MODIFY `siswa_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `tbl_testimoni`
+--
+ALTER TABLE `tbl_testimoni`
+  MODIFY `testimoni_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_tulisan`
+--
+ALTER TABLE `tbl_tulisan`
+  MODIFY `tulisan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
